920b6869ca6bdf3cb26448adeec93732d5d3e414
Cron3x <57831654+Cron3x@users.noreply.github.com>
2023-05-26 15:08:00 +0200

    Update packer.lua
    added Packer

--- nvim/lua/0x7f/packer.lua
+++ nvim/lua/0x7f/packer.lua
@@ -33,6 +33,24 @@ return require('packer').startup(function(use)
 	vim.cmd('colorscheme leaf')
 	--use({ 'rose-pine/neovim', as = 'rose-pine' })
 	--vim.cmd('colorscheme rose-pine')
+    use {
+  	'VonHeikemen/lsp-zero.nvim',
+  	branch = 'v2.x',
+  	requires = {
+    	-- LSP Support
+    	{'neovim/nvim-lspconfig'},             -- Required
+    	{                                      -- Optional
+      	     'williamboman/mason.nvim',
+      	     run = function()
+		pcall(vim.cmd, 'MasonUpdate')
+             end,
+    	},
+    	{'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-
+    	-- Autocompletion
+    	{'hrsh7th/nvim-cmp'},     -- Required
+    	{'hrsh7th/cmp-nvim-lsp'}, -- Required
+    	{'L3MON4D3/LuaSnip'},     -- Required
+     }
+}
 end)
